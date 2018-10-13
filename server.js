e1f52204888f65cc19077ef807ba88971eb755bf
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-10-13 13:56:02 -0700

    edit route and update route are working

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Dependencies
 const express = require('express')
-// const methodOverride = require('method-override')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 const db = mongoose.connection
+// Port
+// Allow use of Heroku's port or your own local port, depending on your environment
 const PORT = process.env.PORT || 3000
 // require the schema
 const Postcard = require('./models/postcards.js')
@@ -15,21 +17,16 @@ const postcards = require('./models/seed.js')
 // const postcardsController = require('./controllers/postcards.js')
 // app.use('/postcards', postcardsController)
 
-// Port
-// Allow use of Heroku's port or your own local port, depending on your environment
-
-
-
-
 // Middleware- after config and dependencies but bedore routes
+// use method override
+// use PUT and DELETE verbs (HTML only allows GET and POST)
+app.use(methodOverride('_method'))
 // use public folder for static asses, like css
 app.use(express.static('public'))
 // populates req.body with parsed info from forms, if no data from forms, it will return an empty object
 app.use(express.urlencoded({extended:false}))
 
-// use method override
-// use PUT and DELETE verbs (HTML only allows GET and POST)
-// app.use(methodOverride('_method'))
+
 
 // middleware for postcardsController
 // app.use('/postcards', postcardsController)
@@ -82,13 +79,20 @@ app.get('/alpacapost/:id', (req, res)=>{
 app.get('/alpacapost/:id/edit', (req, res)=>{
   Postcard.findById(req.params.id, (err, foundPostcard)=>{
     res.render('edit.ejs', {
-      postcard: postcards[req.params.id]
-
+      // postcard: postcards[req.params.id]
+      postcard: foundPostcard
     })
   })
 })
 
-
+// Update edit put route
+app.put('/alpacapost/:id', (req, res)=>{
+  Postcard.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPostcard)=>{
+    res.render('show.ejs', {
+      postcard: updatedPostcard
+    })
+  })
+})
 
 
 
