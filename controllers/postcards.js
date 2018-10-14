22064852ea280328c091816e4f01503667be52f4
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-10-14 13:04:40 -0700

    created gitignore and moved routes to postcards.js

--- controllers/postcards.js
+++ controllers/postcards.js
@@ -1,21 +1,83 @@
 //Dependencies
 const express = require('express');
-const postcards = express.Router();
-const Postcards = require('../models/postcards.js')
-
-// Routes
-// // index
-// postcards.get('/', (req, res)=>{
-//   // Postcard.find({}, (error, allPostcards)=>{
-//     res.render('index.ejs', {
-//       postcards: Postcards
-//     })
-//   // })
-// })
+const alpacapost = express.Router();
+// require the schema
+const Postcard = require('../models/postcards.js');
+// require seed data
+const postcards = require('../models/seed.js')
 
 
+// index
+alpacapost.get('/', (req, res)=>{
+  // find all postcards
+  Postcard.find({}, (error, allPostcards)=>{
+    // render index page
+    res.render('index.ejs', {
+      // passes found postcards to index page
+      postcards: allPostcards
+    })
+  })
+})
+
+
+// SEED ROUTE
+alpacapost.get('/seed', (req, res)=>{
+  Postcard.create(postcards, (err, createdPostcard)=>{
+    res.redirect('/alpacapost')
+  })
+})
+
+// create postcard
+alpacapost.post('/', (req, res)=>{
+  Postcard.create(req.body, (error, createdPostcard)=>{
+    res.redirect('/alpacapost')
+    // res.send('you created a thing!')
+  })
+})
+
+// new
+alpacapost.get('/new', (req, res)=>{
+  res.render('new.ejs')
+})
+
+// show
+alpacapost.get('/:id', (req, res)=>{
+  Postcard.findById(req.params.id, (err, foundPostcard)=>{
+    res.render('show.ejs',
+    {
+      postcard: foundPostcard
+      // postcard: postcards[req.params.id],
+      // index: [req.params.id]
+    })
+  })
+})
+
+// DELETE ROUTE
+alpacapost.delete('/:id', (req, res)=>{
+  Postcard.findByIdAndRemove(req.params.id, (err, data)=>{
+    res.redirect('/alpacapost');//redirect back to store index
+  })
+});
+
+// edit route
+alpacapost.get('/:id/edit', (req, res)=>{
+  Postcard.findById(req.params.id, (err, foundPostcard)=>{
+    res.render('edit.ejs', {
+      // postcard: postcards[req.params.id]
+      postcard: foundPostcard
+    })
+  })
+})
+
+// Update edit put route
+alpacapost.put('/:id', (req, res)=>{
+  Postcard.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPostcard)=>{
+    res.render('show.ejs', {
+      postcard: updatedPostcard
+    })
+  })
+})
 
 
 
-
-module.exports = postcards;
+module.exports = alpacapost;
--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const PORT = process.env.PORT || 3000
 const Postcard = require('./models/postcards.js')
 // require the seed data
 const postcards = require('./models/seed.js')
+//Require Postcards Controller
+const postcardsController = require('./controllers/postcards.js')
 
 
-// Controller Middleware
-// const postcardsController = require('./controllers/postcards.js')
-// app.use('/postcards', postcardsController)
 
 // Middleware- after config and dependencies but bedore routes
 // use method override
@@ -25,82 +24,8 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 // populates req.body with parsed info from forms, if no data from forms, it will return an empty object
 app.use(express.urlencoded({extended:false}))
-
-
-
-// middleware for postcardsController
-// app.use('/postcards', postcardsController)
-
-// index
-app.get('/alpacapost', (req, res)=>{
-  Postcard.find({}, (error, allPostcards)=>{
-    res.render('index.ejs', {
-      postcards: allPostcards
-    })
-  })
-})
-
-// require seed data
-// const seed = require('./models/seed.js');
-
-// SEED ROUTE
-app.get('/alpacapost/seed', (req, res)=>{
-  Postcard.create(postcards, (err, createdPostcard)=>{
-    res.redirect('/alpacapost')
-  })
-})
-
-// create postcard
-app.post('/alpacapost', (req, res)=>{
-  Postcard.create(req.body, (error, createdPostcard)=>{
-    res.redirect('/alpacapost')
-    // res.send('you created a thing!')
-  })
-})
-
-// new
-app.get('/alpacapost/new', (req, res)=>{
-  res.render('new.ejs')
-})
-
-// show
-app.get('/alpacapost/:id', (req, res)=>{
-  Postcard.findById(req.params.id, (err, foundPostcard)=>{
-    res.render('show.ejs',
-    {
-      postcard: foundPostcard
-      // postcard: postcards[req.params.id],
-      // index: [req.params.id]
-    })
-  })
-})
-
-// DELETE ROUTE
-app.delete('/alpacapost/:id', (req, res)=>{
-  Postcard.findByIdAndRemove(req.params.id, (err, data)=>{
-    res.redirect('/alpacapost');//redirect back to store index
-  })
-});
-
-// edit route
-app.get('/alpacapost/:id/edit', (req, res)=>{
-  Postcard.findById(req.params.id, (err, foundPostcard)=>{
-    res.render('edit.ejs', {
-      // postcard: postcards[req.params.id]
-      postcard: foundPostcard
-    })
-  })
-})
-
-// Update edit put route
-app.put('/alpacapost/:id', (req, res)=>{
-  Postcard.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPostcard)=>{
-    res.render('show.ejs', {
-      postcard: updatedPostcard
-    })
-  })
-})
-
+// postcards controller middleware
+app.use('/alpacapost', postcardsController)
 
 
 // Error / success
