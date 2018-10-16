7df322cfdcb3e9c241247e68021616e9a9a5fc10
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-10-16 12:29:17 -0700

    fixed images for showpage on heroku. fixing create user route

--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,7 @@ users.get('/new', (req, res)=>{
 users.post('/', (req, res)=>{
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   User.create(req.body, (err, createdUser)=>{
-    res.redirect('/');
+    res.redirect('/alpacapost');
   })
 })
 
