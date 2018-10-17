2fa136c39a2bb624c0c5f8ed9cfb4bd004c2f11c
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-10-17 13:58:41 -0700

    redirected create new user route to log in page

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ users.get('/new', (req, res)=>{
 users.post('/', (req, res)=>{
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   User.create(req.body, (err, createdUser)=>{
-    res.redirect('/alpacapost');
+    res.redirect('/sessions/new');
     currentUser: req.session.currentUser
   })
 })
