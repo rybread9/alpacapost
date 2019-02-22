1dedcf6d08107372752850fc72a880ff37d5df5f
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2019-02-21 20:47:48 -0800

    added mini-view of postcard to right side on show page

--- models/postcards.js
+++ models/postcards.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const postcardSchema = new mongoose.Schema ({
   frontImage: String,
-  backImage: String,
-  sendToName: String,
-  sendToEmail: String,
+  // backImage: String,
+  // sendToName: String,
+  // sendToEmail: String,
   message: String,
-  sendFromName: String
+  // sendFromName: String
 })
 
 const Postcard = mongoose.model('Postcard', postcardSchema);
